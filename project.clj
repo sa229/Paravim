4eb93bb828b420f55d83e33e887baa95a2a208e0
Samuel Groft <69950970+sa229@users.noreply.github.com>
2022-02-28 13:55:34 -0800

    Delete project.clj

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject paravim "0.18.6-SNAPSHOT"
-  :description "A parasitic editor for Clojure"
-  :url "https://github.com/oakes/Paravim"
-  :license {:name "Public Domain"
-            :url "http://unlicense.org/UNLICENSE"}
-  :repositories [["clojars" {:url "https://clojars.org/repo"
-                             :sign-releases false}]]
-  :clean-targets ^{:protect false} ["target"])
